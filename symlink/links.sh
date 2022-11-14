ff8ed813af8e3ac2c5fa30377e916e99e98bda85
cbellop <cbellop@ripley.com>
2022-11-14 01:36:57 -0300

    feat(symlinks): add a symlinks

---
+++ symlink/links.sh
@@ -0,0 +1,3 @@
+ln -s "$HOME/.dotfiles/shell/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/shell/.zshenv" "$HOME/.zshenv"
+ln -s "$HOME/.dotfiles/editors/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
