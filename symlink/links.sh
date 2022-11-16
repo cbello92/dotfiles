b17933d6c3071015e3cd84bd196c2c4d950689f4
cbellop <cbellop@ripley.com>
2022-11-16 01:49:34 -0300

    feat(symlinks): add a new symlink for theme zsh

--- symlink/links.sh
+++ symlink/links.sh
@@ -1,3 +1,4 @@
 ln -s "$HOME/.dotfiles/shell/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/.dotfiles/shell/.zshenv" "$HOME/.zshenv"
 ln -s "$HOME/.dotfiles/editors/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+ln -s "$HOME/.dotfiles/shell/zsh/themes/cdev.zsh-theme" "$ZSH_CUSTOM/themes/cdev.zsh-theme"
