edc671397b29cfa29e79429172f998c8baeb8f1a
cbellop <cbellop@ripley.com>
2023-04-22 21:45:24 -0400

    feat(symlink): define a new symlink for shortcuts vscode

--- symlink/links.sh
+++ symlink/links.sh
@@ -1,4 +1,5 @@
 ln -s "$HOME/.dotfiles/shell/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/.dotfiles/shell/.zshenv" "$HOME/.zshenv"
 ln -s "$HOME/.dotfiles/editors/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+ln -s "$HOME/.dotfiles/editors/vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
 ln -s "$HOME/.dotfiles/shell/zsh/themes/cdev.zsh-theme" "$ZSH_CUSTOM/themes/cdev.zsh-theme"
